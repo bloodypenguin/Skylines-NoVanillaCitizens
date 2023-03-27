e0d35eaaae205a26eb9805518c7076cc52935962
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2023-03-27 12:16:20 +0200

    Add to source control

---
+++ NoVanillaCitizens/LoadingExtension.cs
@@ -0,0 +1,71 @@
+using System.Reflection;
+using ICities;
+
+namespace NoVanillaCitizens
+{
+    public class LoadingExtension : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            base.OnLevelLoaded(mode);
+            if (mode == LoadMode.NewGame || mode == LoadMode.LoadGame || mode == LoadMode.NewGameFromScenario)
+            {
+                SimulationManager.instance.AddAction(FilterCitizens);
+            }
+        }
+
+        private void FilterCitizens()
+        {
+            var groupCitizensField =
+                typeof(CitizenManager).GetField("m_groupCitizens", BindingFlags.Instance | BindingFlags.NonPublic);
+            var groupCitizens = (FastList<ushort>[]) groupCitizensField.GetValue(CitizenManager.instance);
+            var newGroupCitizens = new FastList<ushort>[840];
+            for (var i = 0; i < 840; i++)
+            {
+                if (groupCitizens[i] is null || groupCitizens[i].m_size == 0)
+                {
+                    newGroupCitizens[i] = groupCitizens[i];
+                    continue;
+                }
+
+                var customCitizens = new FastList<ushort>();
+                for (var index = 0; index < groupCitizens[i].m_size; index++)
+                {
+                    var value = groupCitizens[i].m_buffer[index];
+                    var citizen = PrefabCollection<CitizenInfo>.GetPrefab(value);
+                    if (citizen == null)
+                    {
+                        continue;
+                    }
+
+                    if (!IsWorkshopAsset(citizen))
+                    {
+                        continue;
+                    }
+                    customCitizens.Add(value);
+                }
+
+                var service = PrefabCollection<CitizenInfo>.GetPrefab(groupCitizens[i].m_buffer[0])?.m_class?.m_service;
+                var gender = PrefabCollection<CitizenInfo>.GetPrefab(groupCitizens[i].m_buffer[0])?.m_gender;
+                var agePhase = PrefabCollection<CitizenInfo>.GetPrefab(groupCitizens[i].m_buffer[0])?.m_agePhase;
+                var subCulture = PrefabCollection<CitizenInfo>.GetPrefab(groupCitizens[i].m_buffer[0])?.m_subCulture;
+                if (customCitizens.m_size == 0)
+                {
+                    newGroupCitizens[i] = groupCitizens[i];
+                    UnityEngine.Debug.LogWarning(
+                        $"No Vanilla Citizens: Vanilla citizens will be used for service: {service}, gender: {gender}, agePhase: {agePhase}, subCulture: {subCulture}. No custom prefabs found!");
+                    continue;
+                }
+                UnityEngine.Debug.Log(
+                $"No Vanilla Citizens: preventing vanilla citizens from spawning for service: {service}, gender: {gender}, agePhase: {agePhase}, subCulture: {subCulture}");
+                newGroupCitizens[i] = customCitizens;
+            }
+            groupCitizensField.SetValue( CitizenManager.instance, newGroupCitizens);
+        }
+
+        private static bool IsWorkshopAsset(CitizenInfo citizenInfo)
+        {
+            return citizenInfo.m_isCustomContent || citizenInfo.name.Contains(".");
+        }
+    }
+}
---
+++ NoVanillaCitizens/NoVanillaCitizensMod.cs
@@ -0,0 +1,11 @@
+﻿using ICities;
+
+namespace NoVanillaCitizens
+{
+    public class NoVanillaCitizensMod : IUserMod
+    {
+        public string Name => "No Vanilla Citizens";
+
+        public string Description => "Prevents vanilla citizens from spawning";
+    }
+}
