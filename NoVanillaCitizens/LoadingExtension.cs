99dc53f53576f79c82f8cd1c928ff845ae22fab3
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2023-03-27 12:47:04 +0200

    Add filtering for animals

--- NoVanillaCitizens/LoadingExtension.cs
+++ NoVanillaCitizens/LoadingExtension.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using ICities;
 
@@ -10,11 +11,36 @@ namespace NoVanillaCitizens
             base.OnLevelLoaded(mode);
             if (mode == LoadMode.NewGame || mode == LoadMode.LoadGame || mode == LoadMode.NewGameFromScenario)
             {
-                SimulationManager.instance.AddAction(FilterCitizens);
+                SimulationManager.instance.AddAction(FilterAssets);
             }
         }
 
-        private void FilterCitizens()
+        private static void FilterAssets()
+        {
+            try
+            {
+                UnityEngine.Debug.Log("No Vanilla Citizens: filtering citizens...");
+                FilterCitizens();
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogError("No Vanilla Citizens: an error happened while filtering citizens");
+                UnityEngine.Debug.LogException(e);
+            }
+
+            try
+            {
+                UnityEngine.Debug.Log("No Vanilla Citizens: filtering citizens...");
+                FilterAnimals();
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogError("No Vanilla Citizens: an error happened while filtering animals");
+                UnityEngine.Debug.LogException(e);
+            }
+        }
+
+        private static void FilterCitizens()
         {
             var groupCitizensField =
                 typeof(CitizenManager).GetField("m_groupCitizens", BindingFlags.Instance | BindingFlags.NonPublic);
@@ -61,6 +87,53 @@ namespace NoVanillaCitizens
                 newGroupCitizens[i] = customCitizens;
             }
             groupCitizensField.SetValue( CitizenManager.instance, newGroupCitizens);
+        }
+        
+        private static void FilterAnimals()
+        {
+            var groupAnimalsField =
+                typeof(CitizenManager).GetField("m_groupAnimals", BindingFlags.Instance | BindingFlags.NonPublic);
+            var groupAnimals = (FastList<ushort>[]) groupAnimalsField.GetValue(CitizenManager.instance);
+            var newGroupAnimals = new FastList<ushort>[groupAnimals.Length];
+            for (var i = 0; i < newGroupAnimals.Length; i++)
+            {
+                if (groupAnimals[i] is null || groupAnimals[i].m_size == 0)
+                {
+                    newGroupAnimals[i] = groupAnimals[i];
+                    continue;
+                }
+
+                var customAnimals = new FastList<ushort>();
+                for (var index = 0; index < groupAnimals[i].m_size; index++)
+                {
+                    var value = groupAnimals[i].m_buffer[index];
+                    var animal = PrefabCollection<CitizenInfo>.GetPrefab(value);
+                    if (animal == null)
+                    {
+                        continue;
+                    }
+
+                    if (!IsWorkshopAsset(animal))
+                    {
+                        continue;
+                    }
+                    customAnimals.Add(value);
+                }
+
+                var service = PrefabCollection<CitizenInfo>.GetPrefab(groupAnimals[i].m_buffer[0])?.m_class?.m_service;
+                var subCulture = PrefabCollection<CitizenInfo>.GetPrefab(groupAnimals[i].m_buffer[0])?.m_subCulture;
+                if (customAnimals.m_size == 0)
+                {
+                    newGroupAnimals[i] = groupAnimals[i];
+                    UnityEngine.Debug.LogWarning(
+                        $"No Vanilla Citizens: Vanilla animals will be used for service: {service}, subCulture: {subCulture}. No custom prefabs found!");
+                    continue;
+                }
+                UnityEngine.Debug.Log(
+                $"No Vanilla Citizens: preventing vanilla animals from spawning for service: {service}, subCulture: {subCulture}");
+                newGroupAnimals[i] = customAnimals;
+            }
+            groupAnimalsField.SetValue( CitizenManager.instance, newGroupAnimals);
         }
 
         private static bool IsWorkshopAsset(CitizenInfo citizenInfo)
